44ebfdcc669b577644fbbcd9943b9cc7c0a68cd3
ezqreyes <ezqreyes@gmail.com>
2023-08-24 23:06:45 -0300

    Third commit

---
+++ resources/views/components/main-hero.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="w-full bg-center bg-cover h-128" 
+        style="background-image: url('https://images.unsplash.com/photo-1556761175-b413da4baf72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80');">
+        <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
+            <div class="text-center">
+                <h1 class="text-2xl font-semibold text-white uppercase lg:text-3xl">Build your new 
+                    <span class="text-blue-400 underline">Saas</span>
+                </h1>
+                <button class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase 
+                    transition-colors duration-200 transform bg-blue-600 rounded-md lg:w-auto 
+                    hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                    Start project
+                </button>
+            </div>
+        </div>
+    </div>
+</header>
